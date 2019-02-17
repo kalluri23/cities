b0b5dd14f4367ca1836c2681542e4f0252949b47
Krishna Theja Kalluri <krishnateja.kalluri@gmail.com>
2019-02-17 12:01:08 -0600

    Added MVVM core files

---
+++ Cities/Model/BBCity.swift
@@ -0,0 +1,9 @@
+//
+//  BBCity.swift
+//  Cities
+//
+//  Created by Krishna teja Kalluri on 2/17/19.
+//  Copyright © 2019 backbase. All rights reserved.
+//
+
+import Foundation
---
+++ Cities/ViewModel/BBCitiesListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BBCitiesListViewModel.swift
+//  Cities
+//
+//  Created by Krishna teja Kalluri on 2/17/19.
+//  Copyright © 2019 backbase. All rights reserved.
+//
+
+import UIKit
+
+class BBCitiesListViewModel: NSObject {
+
+}
